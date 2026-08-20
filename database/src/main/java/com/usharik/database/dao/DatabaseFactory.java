382c597897d5c6a5d4ce60584901f3c8b66419c6
usharik <usharik@mail.ru>
2026-08-20 13:38:49 +0200

    fix(build): remove Gradle deprecations

--- database/src/main/java/com/usharik/database/dao/DatabaseFactory.java
+++ database/src/main/java/com/usharik/database/dao/DatabaseFactory.java
@@ -1,7 +1,6 @@
 package com.usharik.database.dao;
 
 import android.content.Context;
-import com.usharik.database.DocumentRepository;
 
 /**
  * Created by macbook on 14/03/2018.
@@ -12,11 +11,5 @@ public class DatabaseFactory {
     /** Returns the shared {@link DocumentDatabase} instance. */
     public static DocumentDatabase provideDocumentDatabase(Context context) {
         return DocumentDatabase.getDocumentDatabase(context);
-    }
-
-    /** @deprecated Use {@link #provideDocumentDatabase(Context)} to share one DB instance. */
-    @Deprecated
-    public static DocumentRepository provideDocumentDb(Context context) {
-        return new DocumentRepository(DocumentDatabase.getDocumentDatabase(context));
     }
 }
